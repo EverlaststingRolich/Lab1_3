20e053e07ff2e8bb320abdd8a6becda31010ebc0
EverlaststingRolich <60112075+EverlaststingRolich@users.noreply.github.com>
2020-11-21 18:31:14 +0300

    final

--- second_window.cpp
+++ second_window.cpp
@@ -56,28 +56,4 @@ void second_window::insert_data(object_collection* collection, int size) const
 }
 
 
-void second_window::insert_data_ms(object_collection* collection, int size) const
-{
-    QStandardItemModel *model = new QStandardItemModel;
-    QStandardItem *item;
-    for (int i=0; i< size; i++)
-    {
-
-        item = new QStandardItem(QString("#" + QString::number(i+1)));
-        model->appendRow(item);
-
-        item = new QStandardItem(QString(QString::fromStdString(collection->get_iterator()[i]->get_name())));
-        model->appendRow(item);
-
-        item = new QStandardItem(QString(QString::number(collection->get_iterator()[i]->get_data("price"))));
-        model->appendRow(item);
-
-        item = new QStandardItem(QString(QString::number(collection->get_iterator()[i]->get_data("new"))));
-        model->appendRow(item);
-
-    }
-    ui->listView->setModel(model);
-}
-
-
 
